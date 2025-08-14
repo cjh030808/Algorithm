bb3b3280ce0377306fff9657ae3a72e809d53e3d
goodjjh1234 <goodjjh1234@gmail.com>
2025-08-14 15:56:33 +0900

    SOLVED: 5582.c

---
+++ 5582.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 4000
+int main() {
+	char first[MAX + 1], second[MAX + 1];
+	scanf("%s", first);
+	scanf("%s", second);
+	int f_len = strlen(first), s_len = strlen(second);
+	int LCS[f_len + 2][s_len + 3];
+
+	int result = 0;
+	for (int i = 0; i <= f_len; i++) {
+		for (int j = 0; j <= s_len; j++) {
+			if (i == 0 || j == 0) LCS[i][j] = 0;
+			else if (first[i-1] == second[j-1]) {
+				LCS[i][j] = LCS[i - 1][j - 1] + 1;
+				if (LCS[i][j] > result) result = LCS[i][j];
+			}
+			else LCS[i][j] = 0;
+		}
+	}
+	printf("%d", result);
+	return 0;
+}
